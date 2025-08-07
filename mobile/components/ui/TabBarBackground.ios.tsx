fd1e34b332bfe186a168dfa586082a03e4502dde
Nick Hallstrom <nick@trycrew.com>
2025-08-07 09:12:11 -0600

    React refresh lint

--- mobile/components/ui/TabBarBackground.ios.tsx
+++ mobile/components/ui/TabBarBackground.ios.tsx
@@ -1,4 +1,3 @@
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { BlurView } from 'expo-blur';
 import { StyleSheet } from 'react-native';
 
@@ -15,7 +14,3 @@ const BlurTabBarBackground = () => {
 };
 
 export default BlurTabBarBackground;
-
-export function useBottomTabOverflow() {
-  return useBottomTabBarHeight();
-}
